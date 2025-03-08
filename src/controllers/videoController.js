3a27df2da92ab64550b71ab15ab1f00901647d3a
HareshWed <hareshgw@gmail.com>
2025-03-08 13:27:04 -0600

    Make it pretty again?

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,121 +1,127 @@
 const supabase = require('../config/supabase');
 
-const {uploadStandard, uploadResumable} = require('./fileUploader');
+const { uploadStandard, uploadResumable } = require('./fileUploader');
 
 const videoController = {
-    async videoUpload(req, res){
-      try{
-        //Get form fields
-        const {title, description} = req.body;
-        const videoFile = req.file;
-      
-        //Check for empty fields
-        if (!videoFile || !title || !description) {
-            return res.status(400).json({ error: 'Error: Title, description, or file upload fields are empty' });
-        }
-      
-        const fileBuffer = videoFile.buffer;
-        const filePath = `Course_videos/${title}`;
-        //const {data, error} = await uploadStandard('Videos', filePath, fileBuffer, videoFile.mimetype);
-        const something = await uploadResumable('Videos', filePath, fileBuffer, videoFile.mimetype);
-        
-        console.log(something);
-        /*
+  async videoUpload(req, res) {
+    try {
+      //Get form fields
+      const { title, description } = req.body;
+      const videoFile = req.file;
+
+      //Check for empty fields
+      if (!videoFile || !title || !description) {
+        return res.status(400).json({
+          error: 'Error: Title, description, or file upload fields are empty',
+        });
+      }
+
+      const fileBuffer = videoFile.buffer;
+      const filePath = `Course_videos/${title}`;
+      //const {data, error} = await uploadStandard('Videos', filePath, fileBuffer, videoFile.mimetype);
+      const something = await uploadResumable(
+        'Videos',
+        filePath,
+        fileBuffer,
+        videoFile.mimetype
+      );
+
+      console.log(something);
+      /*
         if (error) {
           return res.status(400).json({ error: error.message });
         }
         */
-        const urlInfo = await supabase.storage.from('Videos')
+      const urlInfo = await supabase.storage
+        .from('Videos')
         .getPublicUrl(filePath);
-        
-        const url_data = urlInfo.data;
-        const url_error = urlInfo.error;
-        if (url_error) {
-          return res.status(400).json({ error: 'Error: Failed to create url for video upload' });
-        } else {
-          console.log(url_data);
-          console.log('Signed URL:', url_data.publicUrl);
-        }
 
-        //Upload video to videos table
-        const video_link = url_data.publicUrl;
-        const { data: videoData, error: videoError } = await supabase
+      const url_data = urlInfo.data;
+      const url_error = urlInfo.error;
+      if (url_error) {
+        return res
+          .status(400)
+          .json({ error: 'Error: Failed to create url for video upload' });
+      } else {
+        console.log(url_data);
+        console.log('Signed URL:', url_data.publicUrl);
+      }
+
+      //Upload video to videos table
+      const video_link = url_data.publicUrl;
+      const { data: videoData, error: videoError } = await supabase
         .from('videos')
         .insert([
           {
             title,
             description,
-            video_link
+            video_link,
           },
         ])
         .select()
         .single();
 
-  if (videoError) {
-    return res.status(500).json({ message: 'Error inserting data', error: videoError.message });
-  }
+      if (videoError) {
+        return res
+          .status(500)
+          .json({ message: 'Error inserting data', error: videoError.message });
+      }
 
       res.status(200).json({
-        message: 'Video uploaded successfully'
+        message: 'Video uploaded successfully',
       });
-      }
-      catch(error){
-        console.log("An error occured:", error);
-        res
-        .status(500)
-        .json({ error: 'Failed to upload video to server' });
-      }
-    },
+    } catch (error) {
+      console.log('An error occured:', error);
+      res.status(500).json({ error: 'Failed to upload video to server' });
+    }
+  },
 
-    async getVideo(req, res){
-        try{
-          const videoId = req.params.videoId;
-          
-          const {data, error} = await supabase
-          .from('videos')
-          .select('title, description, video_link')
-          .eq('id', videoId)
-          .single();
-          
-          if (error) {
-            return res.status(400).json({ message: 'Invalid video id', error: error.message });
-          }
-          console.log(data);
-          res.status(200).json(data);
-        }
-        catch(error){
-          console.log("An error occured:", error);
-        res
-        .status(500)
-        .json({ error: 'Failed to get video'});
-      }
-    },
-
-    async editVideo(req, res){
+  async getVideo(req, res) {
+    try {
       const videoId = req.params.videoId;
 
-       //Get form fields
-       const {id, title, description} = req.body;
-       const videoFile = req.file;
-     
-       //Check for empty fields
-       if (!videoFile || !title || !description) {
-           return res.status(400).json({ error: 'Error: Title, description, or file upload fields are empty' });
-       }
-     
-       const fileBuffer = videoFile.buffer;
-       const filePath = `Course_videos/${title}`;
-       //Gerardo
-       
-
       const { data, error } = await supabase
-    .from('videos')
-    .update({ title, description, video_link })
-    .eq('id', videoId)
-    .single();
-    
-  
-    }
-  };
+        .from('videos')
+        .select('title, description, video_link')
+        .eq('id', videoId)
+        .single();
 
-  module.exports = videoController;
+      if (error) {
+        return res
+          .status(400)
+          .json({ message: 'Invalid video id', error: error.message });
+      }
+      console.log(data);
+      res.status(200).json(data);
+    } catch (error) {
+      console.log('An error occured:', error);
+      res.status(500).json({ error: 'Failed to get video' });
+    }
+  },
+
+  async editVideo(req, res) {
+    const videoId = req.params.videoId;
+
+    //Get form fields
+    const { id, title, description } = req.body;
+    const videoFile = req.file;
+
+    //Check for empty fields
+    if (!videoFile || !title || !description) {
+      return res.status(400).json({
+        error: 'Error: Title, description, or file upload fields are empty',
+      });
+    }
+
+    const fileBuffer = videoFile.buffer;
+    const filePath = `Course_videos/${title}`;
+
+    const { data, error } = await supabase
+      .from('videos')
+      .update({ title, description, video_link })
+      .eq('id', videoId)
+      .single();
+  },
+};
+
+module.exports = videoController;
